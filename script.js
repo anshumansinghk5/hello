3f4c3ab395d663ec8a8cbeee81a941603a5728ca
anshumansinghk5 <90785582+anshumansinghk5@users.noreply.github.com>
2021-09-15 21:25:48 +0530

    hello

---
+++ script.js
@@ -0,0 +1,43 @@
+const jokes=document.querySelector('#joke');
+const jokebtn=document.querySelector('#jokebtn');
+
+// const generatejokes=()=>
+// {
+//     const setHeader={
+//         headers:{
+//             Accept:"application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com',setHeader)
+//     .then((res)=> res.json())
+//     .then((data)=>{
+//         jokes.innerHTML=data.joke;
+//     })
+//     .catch((error)=>
+//     {
+//         console.log(error);
+//     })
+// } 
+
+
+// jokebtn.addEventListener('click',generatejokes);
+// generatejokes();
+
+const generatejokes=async()=>
+{
+    try{
+    const setHeader={
+                headers:{
+                    Accept:"application/json"
+                }
+            }
+            const res=await fetch('https://icanhazdadjoke.com',setHeader)
+             const data=await res.json();
+            jokes.innerHTML=data.joke;
+}catch(error)
+{
+    console.log(`the erron is ${erron}`);
+}
+}
+jokebtn.addEventListener('click',generatejokes);
+ generatejokes();
